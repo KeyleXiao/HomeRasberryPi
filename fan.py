9dcb45c2e15bcf574e742df73c1e6e4b3881d976
keylexiao <keyle_xiao@hotmail.com>
2017-03-29 12:13:43 +0800

    1

--- fan.py
+++ fan.py
@@ -3,7 +3,12 @@
 
 # 自动风扇控制程序，使用wiringPi的gpio命令来操作GPIO
 
-import commands, time
+import commands, time, os
+
+
+def get_cpu_temperature():
+	return float(os.popen('vcgencmd measure_temp').readline().replace("temp=", "").replace("'C\n", ""))
+
 
 # 控制风扇的GPIO
 FAN_GPIO = 15
@@ -11,17 +16,20 @@ FAN_GPIO = 15
 commands.getoutput('sudo gpio mode ' + str(FAN_GPIO) + ' output')
 
 while True:
-	tmpFile = open('/sys/class/thermal/thermal_zone0/temp')
-cpu_temp_raw = tmpFile.read()
-tmpFile.close()
-cpu_temp = round(float(cpu_temp_raw) / 1000, 1)
+	# tmpFile = open('/sys/class/thermal/thermal_zone0/temp')
+	#
+	# cpu_temp_raw = tmpFile.read()
+	# tmpFile.close()
+	# cpu_temp = round(float(cpu_temp_raw) / 1000, 1)
 
-# 如果温度大于45`C，就启动风扇
-if cpu_temp & gt;=45.0:
-	commands.getoutput('sudo gpio write ' + str(FAN_GPIO) + ' 0')
+	print(cpu_temp)
 
-# 如果温度小于42`C，就关闭风扇
-if cpu_temp & lt;42.0:
-	commands.getoutput('sudo gpio write ' + str(FAN_GPIO) + ' 1')
+	# 如果温度大于45`C，就启动风扇
+	# if cpu_temp & gt;=45.0:
+	# 	commands.getoutput('sudo gpio write ' + str(FAN_GPIO) + ' 0')
+	#
+	# # 如果温度小于42`C，就关闭风扇
+	# if cpu_temp & lt;42.0:
+	# 	commands.getoutput('sudo gpio write ' + str(FAN_GPIO) + ' 1')
 
-time.sleep(5)
+	time.sleep(5)
