c324acc49cfdc06f67deb26eeed04a6bb725f0be
keylexiao <keyle_xiao@hotmail.com>
2017-03-29 12:45:34 +0800

    no message

---
+++ test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python2
+# coding:utf8
+
+# 自动风扇控制程序，使用wiringPi的gpio命令来操作GPIO
+
+import os
+from time import sleep
+
+
+def get_cpu_temperature():
+	return float(os.popen('vcgencmd measure_temp').readline().replace("temp=", "").replace("'C\n", ""))
+
+
+def get_cpu_temperature2():
+	file = open("/sys/class/thermal/thermal_zone0/temp")
+	temp = float(file.read()) / 1000
+	file.close()
+	return temp
+
+
+while True:
+	tp = get_cpu_temperature()
+	print("%d < current cpu temperature" % tp)
+
+	sleep(3)
