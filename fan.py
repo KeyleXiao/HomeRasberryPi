83accc6fcfcc11d2b7c4bed079c58a7d4ad306b8
keylexiao <keyle_xiao@hotmail.com>
2017-03-29 12:18:59 +0800

    no message

--- fan.py
+++ fan.py
@@ -3,13 +3,13 @@
 
 # 自动风扇控制程序，使用wiringPi的gpio命令来操作GPIO
 
-import time, os
-
+import os
+from time import sleep
 
 def get_cpu_temperature():
 	return float(os.popen('vcgencmd measure_temp').readline().replace("temp=", "").replace("'C\n", ""))
 
 
 while True:
-	get_cpu_temperature()
-	time.sleep(5)
+	print("test")
+	sleep(2)
